7bf1c8f0a14d12c93a4e6a2a9d29190362efc5bb
eldelgorrodepesca <jorge.carrilloecobar@gmail.com>
2022-04-21 19:18:22 +0200

    MiComponente fix, Guia fix, URL routing and URL parameters

--- src/Guia.js
+++ src/Guia.js
@@ -18,9 +18,38 @@ class Guia extends Component {
                 <Switch>
                     <Route exact path='/' component={Peliculas} />
                     <Route exact path='/index.html' component={Peliculas} />
-
                     <Route path='/ruta-prueba' component={SeccionPruebas} />
                     <Route path='/segunda-ruta' component={MiComponente} />
+
+                    <Route exact path="/pagina-1" render={() => (
+                        <React.Fragment>
+                            <h1>Hola mundo desde la ruta: PAGINA-1</h1>
+                            <MiComponente saludo="Hola amigo" />
+                        </React.Fragment>
+                    )} />
+
+                    <Route exact path="/pruebas/:nombre/:apellidos?" render={(props) => {
+                        var nombre = props.match.params.nombre;
+                        var apellidos = props.match.params.apellidos;
+
+                        return (
+                            <div id='content'>
+                                <h1 className='subheader'>Pagina de pruebas</h1>
+                                <h2>
+                                    {nombre && !apellidos &&
+                                        <React.Fragment>
+                                            {nombre}
+                                        </React.Fragment>
+                                    }
+                                    {nombre && apellidos &&
+                                        <React.Fragment>
+                                            {nombre} {apellidos}
+                                        </React.Fragment>
+                                    }
+                                </h2>
+                            </div>
+                        )
+                    }} />
 
                     <Route component={Error} />
                 </Switch>
--- src/components/MiComponente.jsx
+++ src/components/MiComponente.jsx
@@ -1,10 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class MiComponente extends Component {
 
     render() {
-        return(
-            <h1>Soy el componente llamado: MiComponente</h1>
+        let receta = {
+            nombre: 'Pizza Prosciutto',
+            ingredientes: ['Tomate', 'Queso', 'Jamon Cocido'],
+            calorias: 400 + 'kcal'
+        }
+
+        return (
+            <div className="mi-componente">
+                <h1>{'Producto: ' + receta.nombre}</h1>
+                <h2>{'Valor nutricional: ' + receta.calorias}</h2>
+                <ol>
+                    {
+                        receta.ingredientes.map((ingrediente, i) => {
+                            return (
+                                <li key={i}>
+                                    {ingrediente}
+                                </li>
+                            )
+                        })
+                    }
+                </ol>
+                <hr />
+
+                {this.props.saludo &&
+                    <React.Fragment>
+                        <h1>Desde una Prop: </h1>
+                        <h3>{this.props.saludo}</h3>
+                    </React.Fragment>
+                }
+            </div>
         );
     }
 }
